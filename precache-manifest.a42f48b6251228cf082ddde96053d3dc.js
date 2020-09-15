07541a0b6c13141a35b163d84c219b0e85c5167a
Radha Venugopal <radha.flex@gmail.com>
2020-09-15 12:08:07 +0200

    Updates

---
+++ precache-manifest.a42f48b6251228cf082ddde96053d3dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6031d4ffa027901eea4fad1c2c557ffb",
+    "url": "/pennywise/index.html"
+  },
+  {
+    "revision": "d5d11384f962a8e1f6c4",
+    "url": "/pennywise/static/css/main.a81ded66.chunk.css"
+  },
+  {
+    "revision": "bf15ebcf321b391f0f95",
+    "url": "/pennywise/static/js/2.366b69d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pennywise/static/js/2.366b69d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d11384f962a8e1f6c4",
+    "url": "/pennywise/static/js/main.6e247487.chunk.js"
+  },
+  {
+    "revision": "0cbae9c0b987a3a5409a",
+    "url": "/pennywise/static/js/runtime-main.22cb1b4f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pennywise/precache-manifest.7e0e5c3267d6f98f39bf30fa25a90a66.js"
+  "/pennywise/precache-manifest.a42f48b6251228cf082ddde96053d3dc.js"
 );
 
 self.addEventListener('message', (event) => {
