51d7a2dcd8b5cf8bdbf8dbf8cad483c93d61ae16
Radha Venugopal <radha.flex@gmail.com>
2020-09-15 11:28:26 +0200

    Updates

---
+++ precache-manifest.7e0e5c3267d6f98f39bf30fa25a90a66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a05b365b69796dda1c145fb07825a9",
+    "url": "/pennywise/index.html"
+  },
+  {
+    "revision": "ce33b8dd5c9c3c82898a",
+    "url": "/pennywise/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c381cba5fae144736bc7",
+    "url": "/pennywise/static/js/2.309516e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pennywise/static/js/2.309516e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce33b8dd5c9c3c82898a",
+    "url": "/pennywise/static/js/main.c15ab849.chunk.js"
+  },
+  {
+    "revision": "0cbae9c0b987a3a5409a",
+    "url": "/pennywise/static/js/runtime-main.22cb1b4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pennywise/static/media/logo.5d5d9eef.svg"
+  }
+]);
